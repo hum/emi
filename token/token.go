6844cd1a9c33cbcfe2c38cf8d0161f64a0e64768
hum <matonohamartin@gmail.com>
2020-12-04 19:12:48 +0100

    support for more keywords

--- token/token.go
+++ token/token.go
@@ -24,6 +24,11 @@ const (
 
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 
 	LT = "<"
 	GT = ">"
@@ -37,8 +42,13 @@ type Token struct {
 }
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
